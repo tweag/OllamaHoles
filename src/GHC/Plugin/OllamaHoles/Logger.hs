f06f95bbbc15a8ec49f5c74c9f0aa3ab1e4a82c9
Nathan Bloomfield <nathan.bloomfield@tweag.io>
2026-04-16 14:35:52 -0500

    Add logger config and session ID

--- src/GHC/Plugin/OllamaHoles/Logger.hs
+++ src/GHC/Plugin/OllamaHoles/Logger.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric #-}
 module GHC.Plugin.OllamaHoles.Logger
     ( Logger()
     , initLogger
@@ -8,27 +9,70 @@ module GHC.Plugin.OllamaHoles.Logger
 
 
 
+import qualified Data.Text as T
+import           Data.Word (Word32)
+import           GHC.Generics (Generic)
+import           Numeric (showHex)
+import           System.Random (randomIO)
+
+
+
 -- | Opaque logging object
 data Logger = Logger
     { logEvent :: LogEvent -> IO ()
+    , config   :: LogConfig
     }
 
 -- | Initialize a logger
 initLogger :: IO Logger
-initLogger = pure $ Logger
-    { logEvent = \_ -> putStrLn "XYZZY"
-    }
+initLogger = do
+    logConfig <- initLogConfig
+    pure $ Logger
+      { logEvent = \_ -> putStrLn "XYZZY"
+      , config   = logConfig
+      }
 
 -- | Invoke a logger
 writeLogEvent :: Logger -> LogEvent -> IO ()
 writeLogEvent logger event = do
-    let Logger write = logger
+    let Logger write config = logger
     write event
 
 
+
+-- Configuration
+
+data LogConfig = LogConfig
+    { sessionId :: SessionId
+    } deriving (Eq, Show, Generic)
+
+initLogConfig :: IO LogConfig
+initLogConfig = do
+    sId <- genSessionId
+    pure $ LogConfig
+        { sessionId = sId
+        }
+
+
+
+-- Events
 
 data LogEvent = LogEvent
 
 -- | Smart constructor
 mkLogEvent :: LogEvent
 mkLogEvent = LogEvent
+
+
+
+-- Helpers
+
+newtype SessionId = SessionId T.Text
+  deriving (Eq, Ord, Show, Generic)
+
+genSessionId :: IO SessionId
+genSessionId = do
+  w <- randomIO :: IO Word32
+  -- Note: showHex is a ShowS, which prepends the (hex
+  -- representation of) the left argument onto the right.
+  pure $ SessionId $ T.pack $ take 8 $ showHex w "00000000"
