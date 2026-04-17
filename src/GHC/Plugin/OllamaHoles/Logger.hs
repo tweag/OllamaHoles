2129752cb45e3611c0a9bf4a6579186b965d98f2
Nathan Bloomfield <nathan.bloomfield@tweag.io>
2026-04-17 14:06:45 -0500

    Add stats to event type and render logs

--- src/GHC/Plugin/OllamaHoles/Logger.hs
+++ src/GHC/Plugin/OllamaHoles/Logger.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module GHC.Plugin.OllamaHoles.Logger
     ( Logger()
     , initLogger
@@ -9,7 +11,18 @@ module GHC.Plugin.OllamaHoles.Logger
 
 
 
+import qualified Crypto.Hash as H
+import           Data.Aeson (encode, FromJSON(..), ToJSON(..), (.:))
+import qualified Data.Aeson as Aeson
+import qualified Data.Aeson.Key as Key
+import qualified Data.Aeson.KeyMap as KM
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import qualified Data.Text.Encoding as TE
+import           Data.Time.Clock (getCurrentTime)
+import           Data.Time.Format (defaultTimeLocale, formatTime)
 import           Data.Word (Word32)
 import           GHC.Generics (Generic)
 import           Numeric (showHex)
@@ -35,11 +48,34 @@ initLogger :: IO Logger
 initLogger = do
     logConfig <- initLogConfig
     pure $ Logger
-      { logEvent = \_ -> putStrLn "XYZZY"
-      , config   = logConfig
-      }
+        { logEvent = recordLogEvent logConfig
+        , config   = logConfig
+        }
 
--- | Invoke a logger
+recordLogEvent :: LogConfig -> LogEvent -> IO ()
+recordLogEvent config event = do
+    now <- getFormattedTimestamp
+    let record = renderLogEvent now config event
+    putStrLn "XYZZY"
+
+renderLogEvent
+    :: Timestamp -> LogConfig -> LogEvent
+    -> (LBS.ByteString, PromptHash, ResponseHash)
+renderLogEvent now config event =
+    let promptHash = contentHash $ lePrompt event
+        responseHash = contentHash $ leResponse event
+        line = encode $ Aeson.Object $ KM.fromList
+            [ (Key.fromText "timestamp"        , toJSON now)
+            , (Key.fromText "session_id"       , toJSON $ unSessionId $ sessionId config)
+            , (Key.fromText "suggestion_count" , toJSON $ leSuggestionCount event)
+            , (Key.fromText "unique_count"     , toJSON $ leUniqueCount event)
+            , (Key.fromText "valid_count"      , toJSON $ leValidCount event)
+            , (Key.fromText "prompt_hash"      , toJSON promptHash)
+            , (Key.fromText "response_hash"    , toJSON responseHash)
+            ]
+    in (line, promptHash, responseHash)
+
+-- | Invoke a logger (this is exposed)
 writeLogEvent :: Logger -> LogEvent -> IO ()
 writeLogEvent logger event = do
     let Logger write config = logger
@@ -91,21 +127,45 @@ ensureLogDirs paths = do
     createDirectoryIfMissing True $ lpEventsDir paths
     createDirectoryIfMissing True $ lpBlobDir paths
 
+getFormattedTimestamp :: IO Timestamp
+getFormattedTimestamp = do
+  now <- getCurrentTime
+  pure . T.pack $ formatTime defaultTimeLocale "%Y-%m-%d_%H:%M:%S" now
+
+contentHash :: T.Text -> T.Text
+contentHash =
+  T.pack . show . (H.hash . TE.encodeUtf8 :: T.Text -> H.Digest H.SHA256)
+
 
 
 -- Events
 
 data LogEvent = LogEvent
+  { lePrompt          :: Prompt
+  , leResponse        :: Response
+  , leSuggestionCount :: SuggestionCount -- returned by the LLM
+  , leUniqueCount     :: UniqueCount     -- deduplicated (llm may be redundant)
+  , leValidCount      :: ValidCount      -- how many unique candidates where valid
+  }
+
+type Prompt          = T.Text
+type Response        = T.Text
+type SuggestionCount = Int
+type UniqueCount     = Int
+type ValidCount      = Int
+type Timestamp       = T.Text
+type PromptHash      = T.Text
+type ResponseHash    = T.Text
 
 -- | Smart constructor
 mkLogEvent :: LogEvent
-mkLogEvent = LogEvent
+mkLogEvent = LogEvent mempty mempty 0 0 0
 
 
 
 -- Helpers
 
-newtype SessionId = SessionId T.Text
+newtype SessionId = SessionId { unSessionId :: T.Text }
   deriving (Eq, Ord, Show, Generic)
 
 genSessionId :: IO SessionId
