757f76c1e94bf20f38d3d3a8e58984bf1297fcdb
Nathan Bloomfield <nathan.bloomfield@tweag.io>
2026-05-12 10:26:04 -0500

    Fix errors

--- spec/GHC/Plugin/OllamaHoles/Candidate/Normalize/Spec.hs
+++ spec/GHC/Plugin/OllamaHoles/Candidate/Normalize/Spec.hs
@@ -173,7 +173,7 @@ withRenamedExpr exts rhs k =
             dflags0 <- getSessionDynFlags
             _ <- setSessionDynFlags dflags0
 
-#if MIN_VERSION_GLASGOW_HASKELL(9,8,0,0)
+#if MIN_VERSION_GLASGOW_HASKELL(9,6,0,0)
             target <- guessTarget fp Nothing Nothing
 #else
             target <- guessTarget fp Nothing
